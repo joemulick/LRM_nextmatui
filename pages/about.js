c489b67c25f4054a455861e1ec5b83be877f7851
JoeMulick <joemulick@gmail.com>
2019-03-27 23:14:34 -0700

    getting ready to style index page

--- pages/about.js
+++ pages/about.js
@@ -2,15 +2,12 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 const styles = theme => ({
   root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
+
   },
 });
 
@@ -19,20 +16,7 @@ function About(props) {
 
   return (
     <div className={classes.root}>
-      <Typography variant="h4" gutterBottom>
-        Material-UI
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom>
-        about page
-      </Typography>
-      <Typography gutterBottom>
-        <Link href="/">
-          <a>Go to the main page</a>
-        </Link>
-      </Typography>
-      <Button variant="contained" color="primary">
-        Do nothing button
-      </Button>
+      
     </div>
   );
 }
